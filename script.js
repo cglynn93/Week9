aadd6f41b0b4dea106b2a6a75b65e19abd504ea0
Chloe <glynnchloe93@gmail.com>
2021-11-24 17:51:05 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function count(){
+  for(i=99;i>=1;i=i-1){
+    document.getElementById("main").innerHTML+="<br />"+i+" bottles of beer on the wall, "+i+" bottles of beer"+"<br />"+"Take one down and pass it around, "+(i-1)+" bottles of beer on the wall.";
+  }
+}
